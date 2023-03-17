7b3eda9bca1f8f60876982101e3e1f8bea26eb46
Eugene Krymov <eugene.krymov@gmail.com>
2023-03-17 13:43:33 +0300

    add config tabstop

--- init.lua
+++ init.lua
@@ -86,5 +86,13 @@ return {
     --     ["~/%.config/foo/.*"] = "fooscript",
     --   },
     -- }
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "cpp", "go", "python" },
+      callback = function()
+        vim.opt_local.tabstop = 4
+        vim.opt_local.shiftwidth = 4
+        vim.opt_local.expandtab = false
+      end,
+    })
   end,
 }
